6aa31bebd8349ad314bb452b53733cefe638b53b
schwarzchauhan <harshchauhan0994@gmail.com>
2023-04-23 23:47:40 +0530

    upadated Upload.sol

--- contracts/Upload.sol
+++ contracts/Upload.sol
@@ -4,47 +4,47 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Upload {
   
-  struct Access{
+  struct AccessStruct{
      address user; 
      bool access; //true or false
   }
-  mapping(address=>string[]) value;
-  mapping(address=>mapping(address=>bool)) ownership;
-  mapping(address=>Access[]) accessList;
-  mapping(address=>mapping(address=>bool)) previousData;
+  mapping(address=>string[]) dataChunks;
+  mapping(address=>mapping(address=>bool)) accessMap; // access  accessMap[u1][u2] : access of content of u1 to u2
+  mapping(address=>AccessStruct[]) allowedUserArr;           // map maintained by owner to keep track of their authorised user list
+  mapping(address=>mapping(address=>bool)) oneTimeMap;  // oneTimeMap[u1][u2] : check if access already given to u2 by u1
 
   function add(address _user,string memory url) external {
-      value[_user].push(url);
+      dataChunks[_user].push(url);
   }
   function allow(address user) external {//def
-      ownership[msg.sender][user]=true; 
-      if(previousData[msg.sender][user]){
-         for(uint i=0;i<accessList[msg.sender].length;i++){
-             if(accessList[msg.sender][i].user==user){
-                  accessList[msg.sender][i].access=true; 
+      accessMap[msg.sender][user]=true; 
+      if(oneTimeMap[msg.sender][user]){
+         for(uint i=0;i<allowedUserArr[msg.sender].length;i++){
+             if(allowedUserArr[msg.sender][i].user==user){
+                  allowedUserArr[msg.sender][i].access=true; 
              }
          }
       }else{
-          accessList[msg.sender].push(Access(user,true));  
-          previousData[msg.sender][user]=true;  
+          allowedUserArr[msg.sender].push(AccessStruct(user,true));  
+          oneTimeMap[msg.sender][user]=true;  
       }
     
   }
   function disallow(address user) public{
-      ownership[msg.sender][user]=false;
-      for(uint i=0;i<accessList[msg.sender].length;i++){
-          if(accessList[msg.sender][i].user==user){ 
-              accessList[msg.sender][i].access=false;  
+      accessMap[msg.sender][user]=false;
+      for(uint i=0;i<allowedUserArr[msg.sender].length;i++){
+          if(allowedUserArr[msg.sender][i].user==user){ 
+              allowedUserArr[msg.sender][i].access=false;  
           }
       }
   }
 
   function display(address _user) external view returns(string[] memory){
-      require(_user==msg.sender || ownership[_user][msg.sender],"You don't have access");
-      return value[_user];
+      require(_user==msg.sender || accessMap[_user][msg.sender],"You don't have access");
+      return dataChunks[_user];
   }
 
-  function shareAccess() public view returns(Access[] memory){
-      return accessList[msg.sender];
+  function shareAccess() public view returns(AccessStruct[] memory){
+      return allowedUserArr[msg.sender];
   }
 }
